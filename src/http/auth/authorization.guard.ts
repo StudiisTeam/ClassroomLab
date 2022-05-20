f26223997436fdc52ecc9fbf1fec980e3be2eb5e
MatheusFontenele <matheus.fontenele@icloud.com>
2022-05-20 18:59:44 -0300

    build: change express-jwt of 7.2 to 6.1

--- src/http/auth/authorization.guard.ts
+++ src/http/auth/authorization.guard.ts
@@ -6,7 +6,7 @@ import {
 } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { GqlExecutionContext } from '@nestjs/graphql';
-import * as jwt from 'express-jwt';
+import jwt from 'express-jwt';
 import { expressJwtSecret } from 'jwks-rsa';
 import { promisify } from 'util';
 
@@ -19,24 +19,26 @@ export class AuthorizationGuard implements CanActivate {
     this.AUTH0_AUDIENCE = this.configService.get(this.AUTH0_AUDIENCE) ?? '';
     this.AUTH0_DOMAIN = this.configService.get(this.AUTH0_DOMAIN) ?? '';
   }
+
   async canActivate(context: ExecutionContext): Promise<boolean> {
     const { req, res } = GqlExecutionContext.create(context).getContext();
 
-    //const checkJwt = promisify();
-    // jwt({
-    //   secret: expressJwtSecret({
-    //     cache: true,
-    //     rateLimit: true,
-    //     jwksRequestsPerMinute: 5,
-    //     jwksUri: `${this.AUTH0_DOMAIN}.well-known/jwks.json`,
-    //   }),
-    //   audience: this.AUTH0_AUDIENCE,
-    //   issuer: this.AUTH0_DOMAIN,
-    //   algorithms: ['RS256'],
-    // }),
+    const checkJwt = promisify(
+      jwt({
+        secret: expressJwtSecret({
+          cache: true,
+          rateLimit: true,
+          jwksRequestsPerMinute: 5,
+          jwksUri: `${this.AUTH0_DOMAIN}.well-known/jwks.json`,
+        }),
+        audience: this.AUTH0_AUDIENCE,
+        issuer: this.AUTH0_DOMAIN,
+        algorithms: ['RS256'],
+      }),
+    );
 
     try {
-      // await checkJwt(req, res);
+      await checkJwt(req, res);
 
       return true;
     } catch (err) {
