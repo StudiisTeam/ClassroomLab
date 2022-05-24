2e254e55d69a2f478fd93860cf353c6238547b13
MatheusFontenele <matheus.fontenele@icloud.com>
2022-05-24 10:55:47 -0300

    feat: ensure listEnrollment method with all relationship

--- src/http/graphql/resolvers/enrollment.resolver.ts
+++ src/http/graphql/resolvers/enrollment.resolver.ts
@@ -1,7 +1,32 @@
-import { Resolver } from '@nestjs/graphql';
+import { UseGuards } from '@nestjs/common';
+import { Parent, Query, ResolveField, Resolver } from '@nestjs/graphql';
+import { AuthorizationGuard } from 'src/http/auth/authorization.guard';
+import { CoursesServices } from 'src/services/courses.service';
+import { EnrollmentServices } from 'src/services/enrollment.service';
+import { StudentsServices } from 'src/services/students.service';
 import { Enrollment } from '../models/enrollment';
 
 @Resolver(() => Enrollment)
 export class EnrollmentResolver {
-  constructor() {}
+  constructor(
+    private enrollmentServices: EnrollmentServices,
+    private courseService: CoursesServices,
+    private studentService: StudentsServices,
+  ) {}
+
+  @Query(() => [Enrollment])
+  @UseGuards(AuthorizationGuard)
+  enrollment() {
+    return this.enrollmentServices.listEnrollment();
+  }
+
+  @ResolveField()
+  student(@Parent() enrollment: Enrollment) {
+    return this.studentService.getStudentById(enrollment.studentId);
+  }
+
+  @ResolveField()
+  course(@Parent() enrollment: Enrollment) {
+    return this.courseService.getCourseById(enrollment.courseId);
+  }
 }
--- src/http/graphql/resolvers/student.resolver.ts
+++ src/http/graphql/resolvers/student.resolver.ts
@@ -1,7 +1,25 @@
-import { Resolver } from '@nestjs/graphql';
+import { UseGuards } from '@nestjs/common';
+import { Parent, Query, ResolveField, Resolver } from '@nestjs/graphql';
+import { AuthorizationGuard } from 'src/http/auth/authorization.guard';
+import { EnrollmentServices } from 'src/services/enrollment.service';
+import { StudentsServices } from 'src/services/students.service';
 import { Student } from '../models/student';
 
 @Resolver(() => Student)
 export class StudentResolver {
-  constructor() {}
+  constructor(
+    private studentService: StudentsServices,
+    private enrollmentService: EnrollmentServices,
+  ) {}
+
+  @Query(() => [Student])
+  @UseGuards(AuthorizationGuard)
+  students() {
+    return this.studentService.listAllStudents();
+  }
+
+  @ResolveField()
+  enrollment(@Parent() student: Student) {
+    return this.enrollmentService.getEnrollmentByStudentId(student.id);
+  }
 }
