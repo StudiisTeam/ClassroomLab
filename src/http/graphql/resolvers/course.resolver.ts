58b35c81d07476bae2d742419b10f4fd3fb3695e
MatheusFontenele <matheus.fontenele@icloud.com>
2022-05-24 10:53:16 -0300

    feat: ensure list and create course methods

--- src/http/graphql/resolvers/course.resolver.ts
+++ src/http/graphql/resolvers/course.resolver.ts
@@ -1,7 +1,23 @@
-import { Resolver } from '@nestjs/graphql';
+import { UseGuards } from '@nestjs/common';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { AuthorizationGuard } from 'src/http/auth/authorization.guard';
+import { CoursesServices } from 'src/services/courses.service';
+import { CreateCourseInput } from '../inputs/create-course-input';
 import { Course } from '../models/course';
 
 @Resolver(() => Course)
 export class CourseResolve {
-  constructor() {}
+  constructor(private courseService: CoursesServices) {}
+
+  @Query(() => [Course])
+  @UseGuards(AuthorizationGuard)
+  course() {
+    return this.courseService.listAllCourses();
+  }
+
+  @Mutation(() => Course)
+  @UseGuards(AuthorizationGuard)
+  createCourse(@Args('data') data: CreateCourseInput) {
+    return this.courseService.createCourse(data);
+  }
 }
--- src/services/courses.service.ts
+++ src/services/courses.service.ts
@@ -1,7 +1,46 @@
 import { Injectable } from '@nestjs/common';
+import slugify from 'slugify';
 import { PrismaService } from 'src/database/prisma/prisma.service';
+
+interface CreateCourseParams {
+  title: string;
+}
 
 @Injectable()
 export class CoursesServices {
   constructor(private prisma: PrismaService) {}
+
+  listAllCourses() {
+    return this.prisma.course.findMany();
+  }
+
+  getCourseById(id: string) {
+    return this.prisma.course.findUnique({
+      where: {
+        id,
+      },
+    });
+  }
+
+  async createCourse({ title }: CreateCourseParams) {
+    const slug = slugify(title, {
+      lower: true,
+    });
+
+    const courseAlreadExists = await this.prisma.course.findUnique({
+      where: {
+        slug,
+      },
+    });
+
+    if (courseAlreadExists) {
+      throw new Error('Course Alread exists');
+    }
+    return this.prisma.course.create({
+      data: {
+        title,
+        slug,
+      },
+    });
+  }
 }
