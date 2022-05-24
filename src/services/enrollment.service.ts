6ebe472b45dc3a08a33b3731ba3c3a2f96ead530
MatheusFontenele <matheus.fontenele@icloud.com>
2022-05-24 10:56:46 -0300

    feat: ensure listEnrollment of enrollment service

--- src/services/enrollment.service.ts
+++ src/services/enrollment.service.ts
@@ -4,4 +4,27 @@ import { PrismaService } from 'src/database/prisma/prisma.service';
 @Injectable()
 export class EnrollmentServices {
   constructor(private prisma: PrismaService) {}
+
+  listEnrollment() {
+    return this.prisma.enrollment.findMany({
+      where: {
+        canceledAt: null,
+      },
+      orderBy: {
+        created_at: 'desc',
+      },
+    });
+  }
+
+  getEnrollmentByStudentId(studentId: string) {
+    return this.prisma.enrollment.findMany({
+      where: {
+        studentId,
+        canceledAt: null,
+      },
+      orderBy: {
+        created_at: 'desc',
+      },
+    });
+  }
 }
